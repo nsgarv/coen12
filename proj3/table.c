630fbd94c2489469fd485fd4b41b92d19308f4cb
Nathan Garvey <garvey.na@gmail.com>
2013-05-04 00:06:24 -0700

    commenting

--- proj3/table.c
+++ proj3/table.c
@@ -11,32 +11,53 @@
 static int findElement(SET *sp, char *elt, bool *found);
 
 struct set {
-	int count;
-	int length;
-	char* *elts;
-	char *flags;
+	int count;     /* number of elements in array */
+	int length;    /* length of allocated array   */
+	char* *elts;   /* allocated array of elements */
+	char *flags;   /* array containing information about state of element array */
 };
-
+/*
+ * Function:    createSet
+ *
+ * Complexity:  O(1)
+ *
+ * Description: Return a pointer to a new set with a maximum capacity of
+ *		MAXELTS.
+ */
 SET *createSet(int maxElts)
 {
 	SET *sp;
 	int i;
+
 	sp = malloc(sizeof(SET));
 	assert(sp != NULL);
+
 	sp->count = 0;
 	sp->length = maxElts;
+
 	sp->elts = malloc(sizeof(char*)*maxElts);
 	assert(sp->elts != NULL);
+
 	sp->flags = malloc(sizeof(char)*maxElts);
 	assert(sp->flags != NULL);
+
 	for(i=0; i < sp->length; i++)
 		sp->flags[i] = EMPTY;
 	return sp;
 }
-
+/*
+ * Function:    destroySet
+ *
+ * Complexity:  O(n)
+ *
+ * Description: Deallocate memory associated with the set pointed to by SP.
+ */
 void destroySet(SET *sp)
 {
     int i;
+
+    assert(sp != NULL);
+
     for (i=0; i < sp->length; i++){
         if(sp->flags[i] == FILLED) 
         	free(sp->elts[i]);
@@ -46,6 +67,13 @@ void destroySet(SET *sp)
     free(sp);
 }
 
+/*
+ * Finction:   Hashfunction
+ *
+ * Complexity:  O()
+ *
+ *
+ */
 unsigned hashString(char *s) {
          unsigned hash = 0;
          while (*s != '\0')
@@ -53,18 +81,32 @@ unsigned hashString(char *s) {
          return hash;
 }
 
+/*
+ * Function:    findElement (private)
+ *
+ * Complexity:  O(log n)
+ *
+ * Description: Use linear probing to check the state of flags array at the location
+ *      ELT hashes to. If flags is empty, returns *FOUND is false. is present in the set SP.
+ *		If present, its location is returned and *FOUND is true.
+ *		If not present, the location where it would be inserted is
+ *		returned and *FOUND is false.
+ */
+
 static int findElement(SET *sp, char *elt, bool *found)
 {
 	int avail = -1;
 	int i;
 	int locn, init; 
+
 	init = hashString(elt)%sp->length;
-	for(i=0; i<sp->length; i++){
+
+	for(i=0; i < sp->length; i++){
 		locn = (init + i)%sp->length;
 		if(sp->flags[locn] == EMPTY){
 			*found = false;
-		if(avail == -1)
-			return locn;
+			if(avail == -1)
+				return locn;
 		return avail;
 		}
 		else if(sp->flags[locn] == FILLED){
@@ -78,23 +120,47 @@ static int findElement(SET *sp, char *elt, bool *found)
 				avail = locn;
 			}
 
-		}
+	}
 	*found = false;
 	return locn;
 }
 
+/*
+ * Function:    numElements
+ *
+ * Complexity:  O(1)
+ *
+ * Description: Return the number of elements in the set pointed to by SP.
+ */
 int numElements(SET *sp)
 {
+	assert(sp != NULL);
 	return sp->count;
 }
 
+/*
+ * Function:    hasElement
+ *
+ * Complexity:  O(log n)
+ *
+ * Description: Check if ELT is present in the set pointed to by SP.
+ */
 bool hasElement(SET *sp, char *elt)
 {
 	bool found;
 	findElement(sp, elt, &found);
 	return found;
 }
-
+/*
+ * Function:    addElement
+ *
+ * Complexity:  O(n)
+ *
+ * Description: Add ELT to the set pointed to by SP, and return whether the
+ *		set changed.  A new element is inserted in its proper place
+ *		to keep the array sorted by moving all higher-indexed
+ *		element up.
+ */
 bool addElement(SET *sp, char *elt)
 {
 	if (sp->count == Max_Unique)
@@ -109,6 +175,15 @@ bool addElement(SET *sp, char *elt)
 	sp->count++;
 	return true;
 }
+/*
+ * Function:    removeElement
+ *
+ * Complexity:  O(n)
+ *
+ * Description: Remove ELT from the set pointed to by SP, and return
+ *		whether the set changed.  A element is deleted by moving
+ *		all higher-indexed elements down.
+ */
 bool removeElement(SET *sp, char *elt)
 {
 	bool found;
