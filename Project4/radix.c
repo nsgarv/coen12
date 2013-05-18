3f870cfbe9def75ccd2eb5845b6ccd4f8edbcaf0
Nathan Garvey <garvey.na@gmail.com>
2013-05-18 14:06:34 -0700

    commented radix sort

--- Project4/radix.c
+++ Project4/radix.c
@@ -5,6 +5,10 @@
 
 int main(void)
 {
+	/* creating list of type DEQUE and bucket of type DEQUE 
+		to be uses in the intermediate steps of sorting. 
+	*/
+
 	DEQUE *list;
 	DEQUE *bucket[10];
 	int x, n, num, count;
@@ -12,8 +16,12 @@ int main(void)
 	r = 10;
 	int numDigits;
 	n = 0;
+
+	/*  Only one list is required and createDeque() allocates memory for the list */
 	list = createDeque();
 
+	/*  there are 10 buckets used in this sorting algorithm 
+		so an array of 10 is allocated */
 	for(i=0; i<10; i++){
 		bucket[i] = createDeque();
 	}
@@ -21,35 +29,38 @@ int main(void)
 	printf("Enter some numbers to radix sort\n");
 	printf("Hit return to store each number\n");
 	printf("Press any key to execute sort\n");
+
+	/*  Takeing in the values from user input and strogint hem in the initial list */
 	while (scanf("%d", &x) == 1){
 		if(x>n)
 			n = x;
 		addLast(list, x);
 	}
-	count = numItems(list);
-	//printf("count is %d\n", count);
-	numDigits = ceil((log(n+1)/log(10)));
-	//printf("numDigits is %d\n", numDigits);
-	//printf("---testprint---");
 
+	/*  getting the number of entries used later in loops */
+	count = numItems(list);
+	numDigits = ceil((log(n+1)/log(10)));
+
+
+	/*  loops over for each decimal place using the largest number as the maximum. 
+		The first while loop takes the numbers and places them into their buckets. */
 	for(k=0; k<(numDigits+1); k++)
 	{
-		//printf("--testprint\n");
-		//printf("k is %d\n", k);
-		//for(i=numItems(list); i<0; i--){
 		while(numItems(list) != 0){
 			num = removeFirst(list);
 			j = (int)(num/pow(10,k))%10;         /* j is the bucket index */
-			//printf("num is %d\n", num);
-			//printf("bucket index is %d\n", j);
 			addLast(bucket[j], num);
 		}	
 		i = 0;
+
+		/*  this second while loop takes the numbers brom their buckets 
+			and adds them back to the list.
+			the process is repeaated until the number of digits is reached. 
+		*/
 		while(i <= count){
 			for(f=0; f < r; f++){
 				while(numItems(bucket[f]) != 0){
 					num = removeFirst(bucket[f]);
-					//printf("num2 is %d\n", num);
 					addLast(list,num);
 				}
 			}
