a5e41c769d85dd98e921b2119453695f00ec04c6
Nathan Garvey <garvey.na@gmail.com>
2013-06-03 20:01:50 -0700

    cleaned up folder

--- proj5/huffman.c
+++ proj5/huffman.c
@@ -12,188 +12,121 @@
 typedef struct tree TREE;
 static int count;
 static TREE *heap[END + 1];
-static TREE *nodes[END + 1];
+static TREE *leaves[END + 1];
 
-int getrc (int x);
-int getlc(int x);
-int getp(int x);
 void insert(TREE *heap[], int count, TREE *x);
-void printHuf(TREE *leaf);
+void printHuf(TREE *leaves);
 TREE *delete(TREE *heap[], int count);
-/*
-int main(int argc, char *argv[])
-{
-		
-	if (argc != 3) {                //check if a text file is specified
-	printf ("failed arguements\n");
-	return (0);
-	}
-		 
-	int count = 0;
-	FILE *fp = fopen(argv[1] , "r");  
-	if (fp == NULL){                  //check if the file has anything in it
-		printf("cant open file\n");
-	return(0);
-	 }
 
-	char word[MAX_WORD_LENGTH];
 
-	while (fscanf(fp, "%s" , word) == 1){ //scnning letters into array word until fstanf returns interger 1
-		count++;
-	}
-	printf("%d total words\n", count);
-}
-*/
 int main(int argc, char *argv[])
 {
 	FILE *fp;
-	char *infile, *outfile;
 	int c; //Holds the values for the characters to be read into.
 	int i; 
-	int counts[END+1] = {0}; //Used to hold the numbers of frequencies of arrays in the tree.
+	int charcount[END+1] = {0}; //Used to hold the numbers of frequencies of arrays in the tree.
 	
 
-	TREE*left;		
-	TREE*right;
-	TREE*newTree;
+	TREE *left;		
+	TREE *right;
+	TREE *node;
 
-	infile = argv[1];
-	outfile = argv[2];
 	if (argc != 3) {                //check if a text file is specified
 		printf ("failed arguements\n");
-		return (0);
+		return (1);
 	}
 
-	fp = fopen(infile , "r");  
+	fp = fopen(argv[1] , "r");  
 	if (fp == NULL){                  //check if the file has anything in it
 		printf("Can't open file\n");
-	return(0);
+		return(1);
 	}
+
+	/* count the number of occurances of each character in the file */
 	while((c = getc(fp))!=EOF){
-		counts[c]++;
+		charcount[c]++;
 	}
 	fclose(fp);
 
 	//TREE *createTree(int data, TREE *left, TREE *right)
+	/* create a binary tree of leaves of each character with a non zero count.
+	   Insert all trees into a priority queue. */
 	for (i=0; i <END; i++){
-		if (counts[i] != 0)
-			nodes[i] = createTree(counts[i], NULL, NULL);  
+		if (charcount[i] != 0)
+			leaves[i] = createTree(charcount[i], NULL, NULL);  
 	}
-	nodes[END] = createTree(0,NULL,NULL);
+	leaves[END] = createTree(0,NULL,NULL); //End file marker
 
 	for(i=0; i<=END+1; i++){
-		if(nodes[i] != NULL){
-			insert(heap,count,nodes[i]);
+		if(leaves[i] != NULL){
+			insert(heap,count,leaves[i]);
 		}
 	}
-
+	/* while the priotiry queue has more than one tree in it, remove the two lowest priority trees.
+	   Create a node with each of these trees as subtrees. 
+	   The count for the new tree is the sum of the counts of the two subtrees. 
+	   Insert the new tree into the priority queue. */
 	while(count > 1){
 		left = delete(heap,count);
 		right = delete(heap,count);
-		newTree = createTree(getData(left) + getData(right), left, right);
-		insert(heap, count, newTree);
+		node = createTree(getData(left) + getData(right), left, right);
+		insert(heap, count, node);
 	}
 
 	for(i=0; i<END; i++){
-		if(counts[i] > 0){
+		if(charcount[i] > 0){
 			if(isprint(i) != 0){
-				printf(":'%c' : %d", i, counts[i]);
-				printHuf(nodes[i]);
+				printf("'%c' : %d", i, charcount[i]);
+				printHuf(leaves[i]);
 				printf("\n");
 			}else{
-				printf("%03o : %d ", i, counts[i]);
-				printHuf(nodes[i]);
+				printf("%03o : %d ", i, charcount[i]);
+				printHuf(leaves[i]);
 				printf("\n");
 			}
 		}
 	}
-	printf("%03o : %d", END, counts[END]);
-	printHuf(nodes[END]);
+	printf("%03o : %d", END, charcount[END]);
+	printHuf(leaves[END]);
 	printf("\n");
-	pack(infile, outfile, nodes);
+	pack(argv[1], argv[2], leaves);
 	return(0);
 }
-
-
-
 /*
- * Funtion: 
+ * Funtion: delete()
  *
- * Complexity:
+ * Complexity: O(n)
  *
  * Description: 
  *
  */
-int getr(int x)
-{
-	return r(x);
-}
 
-/*
- * Funtion: 
- *
- * Complexity:
- *
- * Description: 
- *
- */
-int getl(int x)
+ TREE *delete(TREE *heap[], int count)
 {
-	return l(x);
-}
-
-/*
- * Funtion: 
- *
- * Complexity:
- *
- * Description: 
- *
- */
-int getp(int x)
-{
-	return p(x);
-}
-
-/*
- * Funtion: 
- *
- * Complexity:
- *
- * Description: 
- *
- */
-TREE *delete(TREE *heap[], int count)// This delete of struct tree deletes the values from the heap.
-{
+	//Initialize variable, tree to delete (from front), tree to move (last)
 	int i = 0;
-	TREE *x;
-	TREE *min;
-	int child; //heap index location of child
-	
-		
-	min = heap[0];
-	x = heap[ -- count];
-	while(getr(i) < count) 
+	int child;
+	TREE *temp, *del;
+	del = heap[0];
+	temp = heap[--count];
+	//Perform Deletion
+	while ((r(count)) < count)
 	{
-		child = getr(i);
-		if(getl(i) < count && getData(heap[getr(i)]) > getData(heap[getl(i)]))
-		{
-			child = getl(i);
+		int left = l(count);
+		child = left;
+		if (l(count) < count && (getData(heap[l(count)])) < getData(heap[r(count)])){
+			child = left + 1;
 		}
-		if(getData(x) > getData(heap[child]))
-		{
+		if (getData(temp) > getData(heap[child])) {
 			heap[i] = heap[child];
-			i = child;
 		}
 		else
-		{
-			break;
-		}
-	}
-	heap[i] = x;
-	return min;
+			break;	
+			}
+	//Decrement Count		
+	count--;
+	return del;
 }
-
 /*
  * Funtion: 
  *
@@ -202,27 +135,26 @@ TREE *delete(TREE *heap[], int count)// This delete of struct tree deletes the v
  * Description: 
  *
  */
-void insert(TREE *heap[], int count, TREE *x)
+
+void insert(TREE *heap[], int count, TREE *node)
 {
+	//Change the count initialize variables
 	int i;
+	//Perform swaps add the new node in the appropriate arranged location
 	i = count++;
-	while( i>0 &&  getData(heap[getp(i)]) > getData(x))/* While the i of the loop is greater 
-															   than zero and the data of the parent 
-															   of i is greater then the input of the 
-															   tree from which is being passed into. */
-	{
-		heap[i] = heap[getp(i)];/* this switches the parent with the data 
-										in the tree until the node with the smalles
-										value in the root. */
-		i = getp(i);
+	while (i > 0) {
+		if (getData(heap[p(i)]) > getData(node)) {
+			heap[i] = heap[p(i)];
+			i = p(i);
+		}
 	}
-	heap[i] = x;
+	heap[i] = node;
 }
 
 /*
- * Funtion: 
+ * Funtion: PrintHuf()
  *
- * Complexity:
+ * Complexity: 
  *
  * Description: 
  *
